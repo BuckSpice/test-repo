8009af4c63582b2e043871f2dcb43e11c09903e2
buckspice <sharbuck93@yahoo.com>
2022-10-12 12:49:36 -0700

    fixed 1st popup problem. new l2 popup problem

--- first html/home.js
+++ first html/home.js
@@ -1,5 +1,4 @@
-const overlay = document.querySelector("#overlay");
-
+const overlay = document.querySelector(".overlay");
 
 document.querySelector("#show-modal-l1")
 .addEventListener("click", () => {overlay.style.display = "block";
@@ -8,3 +7,40 @@ document.querySelector("#show-modal-l1")
 document.querySelector("#close-modal-l1").
 addEventListener("click", () => {overlay.style.display = "none";
 })
+
+document.querySelector("#show-modal-l2")
+.addEventListener("click", () => {overlay.style.display = "block";
+})
+
+document.querySelector("#close-modal-l2").
+addEventListener("click", () => {overlay.style.display = "none";
+})
+/* i made the id's for overlay into class, i think it'll help later when i figure out this problem
+with the l2 popup showing up as l1, maybe. */
+
+
+// LOGIN JS PAGE//
+
+function myfunction(){
+    var x =document.getElementById("pass");
+
+    if(x.type === "password"){
+        x.type = "text";
+    }
+    else{
+        x.type = "password";
+    }
+}
+function validate(){
+    var password = document.getElementById("pass");
+    var length = document.getElementById("length");
+
+    if(password.Value.length >= 8){
+        alert("Login Successfull");
+        window.location.replace("index.html");
+        return false;
+    }
+    else{
+        alert("Login Failed");
+    }
+}
